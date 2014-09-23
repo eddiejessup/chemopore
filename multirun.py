13f53a7e3d2fa98531cbcde7503e6f6d476c6070
Elliot Marsden <elliot.marsden@gmail.com>
2014-09-23 15:38:09 +0100

    Fix Python bug where pool does not abort via KeyboardInterrupt

--- multirun.py
+++ multirun.py
@@ -12,7 +12,7 @@ def iterate(runner, n_iterations):
 def pool_run(runners, n_iterations):
     iterate_partial = partial(iterate, n_iterations=n_iterations)
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)
-    pool.map(iterate_partial, runners)
+    pool.map_async(iterate_partial, runners).get(1e100)
     pool.close()
     pool.join()
 
